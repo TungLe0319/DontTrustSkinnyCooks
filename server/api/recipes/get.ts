c8df7aea99935cabce35016a9dae1bed816d997f
tungle0319 <tung.le0319@gmail.com>
2023-12-21 06:48:51 -0700

    fixed trouble on mac

--- server/api/recipes/get.ts
+++ server/api/recipes/get.ts
@@ -4,16 +4,17 @@ import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (event) => {
   try {
+    
     const recipes = await prisma().recipe.findMany({
-     include:{
-      user:true,
-      categories:true
-     }
+      include: {
+        categories:true,
+        user:true
+      },
     });
 
     return recipes;
   } catch (error) {
     console.error("Error creating recipe:", error);
-  } 
+  }
 });
 // /api/recipes/post.ts
