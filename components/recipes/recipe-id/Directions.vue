1d16eaed41d163dca521789eef680e1e05cb0bff
TungLe0319 <tung.le0319@gmail.com>
2023-12-23 09:33:43 -0700

    recipe directions alteration

--- components/recipes/recipe-id/Directions.vue
+++ components/recipes/recipe-id/Directions.vue
@@ -12,9 +12,9 @@ defineProps(['directions'])
       </h2>
     </div>
     <ul class="">
-      <li v-for="(direction, index) in JSON.parse(directions)" class="flex flex-col my-3">
-        <span class="font-extrabold">Step {{ index + 1 }} </span>
-        <span>
+      <li v-for="(direction, index) in JSON.parse(directions)" class="flex flex-col my-5">
+        <span class="font-extrabold text-xl">Step {{ index + 1 }} </span>
+        <span class="text-lg">
           {{ direction }}
         </span>
       </li>
