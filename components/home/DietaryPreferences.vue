cae5ad871ba1e8f9318110e09a74ae7307815348
tungle0319 <tung.le0319@gmail.com>
2024-01-03 19:50:00 -0700

    added filter buttons for Dietary Preferences

--- components/home/DietaryPreferences.vue
+++ components/home/DietaryPreferences.vue
@@ -1,23 +1,79 @@
 <script lang="ts" setup>
-import SectionHeader from '../globals/SectionHeader.vue'
-import RecipeCard from '~/components/globals/RecipeCard.vue'
-import type { RecipeWithUserAndCategories } from '~/types/types'
+import { ref } from 'vue';
+import SectionHeader from '../globals/SectionHeader.vue';
+import RecipeCard from '~/components/globals/RecipeCard.vue';
+import type { Category, Prisma } from '@prisma/client';
 
-const { data }
-  = await useFetch<RecipeWithUserAndCategories[]>('/api/recipes')
+const { data: Recipes } = await useFetch<Prisma.RecipeGetPayload<
+{
+  include: {
+      user: true,
+      categories: true,
+      _count: {
+        select: {
+          reviews: true,
+        },
+      },
+      reviews: {
+        select: {
+          id: true,
+          rating: true,
 
-const recipes = data.value?.slice(0, 6)
-const recipesStuff = ref(data.value)
+        },
+      },
+    },
+}>[]>('/api/recipes');
+const recipes = ref(Recipes.value);
+
+const { data: Categories } = await useFetch<Category[]>('/api/categories', {
+  method: 'GET',
+});
+
+const includedCategories = ["Dairy-Free", "Vegetarian", "Vegan", "Gluten-Free", "Nut-Free","Keto-Friendly"];
+const categories = Categories.value?.filter(c => includedCategories.includes(c.name));
+
+const selectedCategory = ref<string | null>(null);
+
+const filterRecipes = () => {
+  if (selectedCategory.value) {
+    recipes.value = Recipes.value?.filter(recipe => {
+      return recipe.categories.some(category => category.name === selectedCategory.value);
+    })?.slice(0, 6);
+  } else {
+    recipes.value = Recipes.value?.slice(0, 6);
+  }
+};
+
+const allCategoryName = "All"; // Assuming "All" is the name for displaying all recipes
+
+// Add "All" category to the beginning of the categories list
+categories?.unshift({ id: 123124124, name: allCategoryName });
+
+const selectCategory = (categoryName: string) => {
+  if (categoryName === allCategoryName) {
+    selectedCategory.value = null; // Reset filter for "All"
+  } else {
+    selectedCategory.value = categoryName;
+  }
+  filterRecipes();
+};
 </script>
+
 
 <template>
   <div class="">
     <SectionHeader title="Recipes Based On Dietary Preferences" />
   
+    <div class="mb-5 mt-2 flex items-center justify-center gap-3 flex-wrap">
+        <UButton v-for="category in categories"   :key="category.id" @click="selectCategory(category.name)">
+          {{ category.name }}
+        </UButton>
+      </div>
+
 
      <Swiper
       :modules="[SwiperAutoplay]"
-      :slides-per-view="4"
+      :slides-per-view="3"
       :loop="true"
  
       :autoplay="{
@@ -25,9 +81,9 @@ const recipesStuff = ref(data.value)
         disableOnInteraction: true,
       }"
 
-      class="w-full "
+      class="w-full h-[500px] "
     >
-      <SwiperSlide v-for="recipe in recipesStuff" :key="recipe.id" class="mx-2">
+      <SwiperSlide v-auto-animate v-for="recipe in  recipes" :key="recipe.id" class="mx-2">
          <RecipeCard :recipe="recipe" />
       </SwiperSlide>
     </Swiper>
