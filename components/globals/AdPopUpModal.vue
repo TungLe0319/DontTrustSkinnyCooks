d85e812944b9100eb71d812f4238644a5ebc4883
TungLe0319 <tung.le0319@gmail.com>
2024-01-07 14:29:03 -0700

    added in signIn button for AdPopUpModal

--- components/globals/AdPopUpModal.vue
+++ components/globals/AdPopUpModal.vue
@@ -25,9 +25,12 @@ watchEffect(() => {
             <!-- <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class=" absolute top-3 right-0" @click="isOpen = false" /> -->
         </div>
        <div class="text-center">
-      <h2 class="text-2xl font-bold mb-2">Unlock Exclusive Content!</h2>
-      <p class="text-gray-600 mb-4">Sign up now to access premium recipes and cooking tips.</p>
-      <UButton size="xl" class="" @click="signIn"> Sign Up</UButton>
+      <h2 class="text-2xl font-bold mb-2">Free Recipe Limit Hit...</h2>
+      <p class="text-gray-600 mb-4"> You've explored some recipes. Now it's time for a more personalized experience, sign in or sign up to save your favorite recipes and contribute your own!</p>
+    <div class="flex items-center justify-center gap-4">
+        <UButton size="xl" class="" @click="signIn"> Sign Up</UButton>
+        <UButton size="xl"  variant="outline" @click="signIn"> Sign In</UButton>
+    </div>
     </div>
       </div>
     </UModal>
